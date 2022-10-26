a4e40fcc22e331d8080446a54704da8165a4150c
naim71 <naimkhan1314@gmail.com>
2022-10-27 05:02:50 +0600

    data added

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/course.json');
+
+
 app.get('/', (req, res) =>{
     res.send('Course API server is Running');
+})
+app.get('/courses-categories', (req, res) =>{
+    res.send(courses);
+})
+app.get('/courses-categories/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+
+    const course = courses.find(cs => cs.id === id) || {};
+    res.send(course);
 })
 
 app.listen(port, () => console.log('Server is Running on port: ', port));
